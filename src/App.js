cec8ae343ade167463a5a03894131f9ebb026ab6
Esakki E <esakki.murugan@vicetrip.com>
2019-10-06 10:08:51 +0530

    Web Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo_full.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://vicetrip.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
